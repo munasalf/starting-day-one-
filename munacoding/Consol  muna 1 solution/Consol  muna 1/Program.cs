c0142080993dd892f2837a6ba463c0810c55554d
MUNA <munaalforqani4@gmail.com>
2023-11-13 16:31:03 +0400

    first commit

---
+++ munacoding/Consol  muna 1 solution/Consol  muna 1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace consol__muna_1
+{
+    class program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Hello,world!");
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
